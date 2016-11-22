a891e30b0b241d20416f993200c6ddd76c43ff67
SerkoUA <serko_x@ukr.net>
2016-11-22 14:15:02 +0200

    urls.py - видає помилку

--- DJ_1/urls.py
+++ DJ_1/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'students.views.students_list', name='home'),
     url(r'^admin/', admin.site.urls),
 ]
